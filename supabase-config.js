5227d5ffaea061960c52ad0cfa666c671554f890
MyShortGame <mitraserviceac04@gmail.com>
2026-09-05 18:37:53 +0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-const SUPABASE_URL="https://jywzhwvbyifulqqvgxxd.supabase.co";
-const SUPABASE_ANON_KEY="sb_publishable_2leqwU4YWbTg--GCIfQMmA_zOEKKG60"";
-const supabaseClient=window.supabase.createClient(SUPABASE_URL,SUPABASE_ANON_KEY,{auth:{persistSession:true,autoRefreshToken:true}});
